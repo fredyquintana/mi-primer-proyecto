cd55b5c381eb332315d615085ff1309925d7c615
fredyquintana <fquintana001@alumno.uaemex.mx>
2023-08-13 19:48:03 -0600

    ejemplo1

---
+++ ProgramaEncapsulamiento.java
@@ -0,0 +1,15 @@
+public class ProgramaEncapsulamiento {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan", 30);
+
+        // Accediendo a los atributos encapsulados
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+
+        // Modificando la edad utilizando el método setter
+        persona1.setEdad(35);
+
+        // Mostrando la información de la persona
+        persona1.mostrarInformacion();
+    }
+}
